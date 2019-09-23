aa0837443ceb5db9f9c282ad9dc7495bcf2be909
shaimoria <shai.moria@zooz.com>
2019-09-23 13:04:08 +0300

    testing

--- out_sqs.go
+++ out_sqs.go
@@ -33,25 +33,29 @@ func FLBPluginInit(plugin unsafe.Pointer) int {
 	return output.FLB_OK
 }
 
-// FLBPluginFlushCtx is called by fluentbit
-func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int {
-	// Type assert context back into the original type for the Go variable
-	queueURL := output.FLBPluginGetContext(ctx).(string)
-	printLog(fmt.Sprintf("flash called. queueURL = %s", queueURL))
+//export FLBPluginFlush
+func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
+	var count int
+	var ret int
+	var ts interface{}
+	var record map[interface{}]interface{}
 
+	// Create Fluent Bit decoder
 	dec := output.NewDecoder(data, int(length))
 
-	count := 0
+	// Iterate Records
+	count = 0
 	for {
-		ret, ts, record := output.GetRecord(dec)
+		// Extract Record
+		ret, ts, record = output.GetRecord(dec)
 		if ret != 0 {
 			break
 		}
 
 		// Print record keys and values
 		timestamp := ts.(output.FLBTime)
-		fmt.Printf("[%d] %s: [%s, {", count, C.GoString(tag), timestamp.String())
-
+		fmt.Printf("[%d] %s: [%s, {", count, C.GoString(tag),
+			timestamp.String())
 		for k, v := range record {
 			fmt.Printf("\"%s\": %v, ", k, v)
 		}
@@ -59,8 +63,12 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 		count++
 	}
 
+	// Return options:
+	//
+	// output.FLB_OK    = data have been processed.
+	// output.FLB_ERROR = unrecoverable error, do not try this again.
+	// output.FLB_RETRY = retry to flush later.
 	return output.FLB_OK
-
 }
 
 // FLBPluginExit is called by fluentBit when shuting down
