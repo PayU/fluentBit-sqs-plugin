9ddde9435afd4f9c43baa99ab0220778c7b4557b
shaimoria <shai.moria@zooz.com>
2019-09-23 13:24:21 +0300

    testing

--- out_sqs.go
+++ out_sqs.go
@@ -5,35 +5,21 @@ import (
 	"fmt"
 	"unsafe"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/fluent/fluent-bit-go/output"
 )
 
-var mySQS *sqs.SQS
-var QueueURL string
-
-// FLBPluginRegister is called by fluentbit
+//export FLBPluginRegister
 func FLBPluginRegister(def unsafe.Pointer) int {
-	return output.FLBPluginRegister(def, "sqs", "Aws SQS")
+	return output.FLBPluginRegister(def, "gstdout", "Stdout GO!")
 }
 
-// FLBPluginInit is called by fluentbit
+//export FLBPluginInit
+// (fluentbit will call this)
 // plugin (context) pointer to fluentbit context (state/ c code)
 func FLBPluginInit(plugin unsafe.Pointer) int {
 	// Example to retrieve an optional configuration parameter
-	QueueURL = output.FLBPluginConfigKey(plugin, "QueueUrl")
-	fmt.Printf("[flb-go-sqs] plugin parameter = '%s'\n", QueueURL)
-
-	mySession, err := session.NewSession()
-
-	if err != nil {
-		fmt.Println("Error", err)
-		return output.FLB_ERROR
-	}
-
-	mySQS = sqs.New(mySession)
+	param := output.FLBPluginConfigKey(plugin, "param")
+	fmt.Printf("[flb-go] plugin parameter = '%s'\n", param)
 	return output.FLB_OK
 }
 
@@ -43,15 +29,10 @@ func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
 	var ret int
 	var ts interface{}
 	var record map[interface{}]interface{}
-	var sqsRecord *sqs.SendMessageBatchRequestEntry
-	var sqsBatch sqs.SendMessageBatchInput
-	var sqsRecords []*sqs.SendMessageBatchRequestEntry
-
-	fmt.Printf("[flb-go-sqs] on FLBPluginFlush")
 
 	// Create Fluent Bit decoder
 	dec := output.NewDecoder(data, int(length))
-	sqsBatch = sqs.SendMessageBatchInput{}
+
 	// Iterate Records
 	count = 0
 	for {
@@ -63,27 +44,13 @@ func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
 
 		// Print record keys and values
 		timestamp := ts.(output.FLBTime)
-		recordString := fmt.Sprintf("{\"tag\":\"%s\", \"timestamp\":\"%s\",", C.GoString(tag),
+		fmt.Printf("[%d] %s: [%s, {", count, C.GoString(tag),
 			timestamp.String())
 		for k, v := range record {
-			recordString = recordString + fmt.Sprintf("\"%s\": %v, ", k, v)
+			fmt.Printf("\"%s\": %v, ", k, v)
 		}
-		recordString = recordString + fmt.Sprintf("}\n")
-
-		sqsRecord = &sqs.SendMessageBatchRequestEntry{
-			Id:          aws.String(fmt.Sprintf("Message No: %d", count)),
-			MessageBody: aws.String(recordString),
-		}
-		sqsRecords = append(sqsRecords, sqsRecord)
+		fmt.Printf("}\n")
 		count++
-		if count%10 == 0 {
-			sqsBatch = sqs.SendMessageBatchInput{
-				Entries:  sqsRecords,
-				QueueUrl: aws.String(QueueURL),
-			}
-			mySQS.SendMessageBatch(&sqsBatch)
-			sqsRecords = nil
-		}
 	}
 
 	// Return options:
