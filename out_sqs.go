eedc6c704159bfb502ea5a7deca8a5f7509b06f2
shaimoria <shai.moria@zooz.com>
2019-09-23 14:03:53 +0300

    yalla yalla

--- out_sqs.go
+++ out_sqs.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"C"
-	"fmt"
+	"log"
 	"unsafe"
 
 	"github.com/fluent/fluent-bit-go/output"
 )
 
-// export FLBPluginRegister
+// FLBPluginRegister is called by fluentBit
 func FLBPluginRegister(def unsafe.Pointer) int {
 	return output.FLBPluginRegister(def, "sqs", "AWS SQS Output plugin")
 }
 
-//export FLBPluginInit
+// export FLBPluginInit
 func FLBPluginInit(plugin unsafe.Pointer) int {
 	QueueURL := output.FLBPluginConfigKey(plugin, "QueueURL")
-	writeLog(fmt.Sprintf("QueueURL: %s", QueueURL))
+	// writeLog(fmt.Sprintf("QueueURL: %s", QueueURL))
 
 	// Set the context to point to any Go variable
 	output.FLBPluginSetContext(plugin, unsafe.Pointer(&QueueURL))
@@ -25,7 +25,7 @@ func FLBPluginInit(plugin unsafe.Pointer) int {
 
 //export FLBPluginFlush
 func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
-	fmt.Print("Flush called for unknown instance")
+	log.Print("Flush called for unknown instance")
 	return output.FLB_OK
 }
 
@@ -33,7 +33,7 @@ func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
 func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int {
 	// Cast context back into the original type for the Go variable
 	QueueURL := (*string)(ctx)
-	fmt.Printf("Flush called for id: %s", *QueueURL)
+	log.Printf("Flush called for id: %s", *QueueURL)
 
 	dec := output.NewDecoder(data, int(length))
 
@@ -53,7 +53,7 @@ func FLBPluginExit() int {
 }
 
 func writeLog(message string) {
-	fmt.Printf("[out-sqs] %s\n", message)
+	log.Printf("[out-sqs] %s\n", message)
 }
 
 func main() {
