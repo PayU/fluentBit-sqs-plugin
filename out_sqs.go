49b4bdd249d631e7640a1bccf673cab38a53a422
shaimoria <shai.moria@zooz.com>
2019-09-23 14:50:27 +0300

    yallalalala

--- out_sqs.go
+++ out_sqs.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"C"
+	"errors"
+	"fmt"
 	"unsafe"
 
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/fluent/fluent-bit-go/output"
 )
 
-// FLBPluginRegister shai
+type sqsConfig struct {
+	queueURL string
+	mySQS    *sqs.SQS
+}
+
+//export FLBPluginRegister
 func FLBPluginRegister(def unsafe.Pointer) int {
 	return output.FLBPluginRegister(def, "sqs", "aws sqs output plugin")
 }
@@ -15,22 +24,49 @@ func FLBPluginRegister(def unsafe.Pointer) int {
 //export FLBPluginInit
 func FLBPluginInit(plugin unsafe.Pointer) int {
 	queueURL := output.FLBPluginConfigKey(plugin, "QueueUrl")
+	writeInfoLog(fmt.Sprintf("queueURL is: %s", queueURL))
 
 	if queueURL == "" {
+		writeErrorLog(errors.New("QueueUrl configuration key is mandatory"))
 		return output.FLB_ERROR
 	}
+
+	myAWSSession, err := session.NewSession()
+	if err != nil {
+		writeErrorLog(err)
+		return output.FLB_ERROR
+	}
+
+	config := sqsConfig{
+		queueURL: queueURL,
+		mySQS:    sqs.New(myAWSSession),
+	}
+
+	// Set the context to point to any Go variable
+	output.FLBPluginSetContext(plugin, config)
 
 	return output.FLB_OK
 }
 
 //export FLBPluginFlushCtx
 func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int {
+	// Type assert context back into the original type for the Go variable
+	id := output.FLBPluginGetContext(ctx).(string)
+	fmt.Printf("[multiinstance] Flush called for id: %s", id)
 	return output.FLB_OK
 }
 
 //export FLBPluginExit
 func FLBPluginExit() int {
 	return output.FLB_OK
+}
+
+func writeInfoLog(message string) {
+	fmt.Printf("[sqs-out] %s", message)
+}
+
+func writeErrorLog(err error) {
+	fmt.Println("[sqs-out] ", err)
 }
 
 func main() {
