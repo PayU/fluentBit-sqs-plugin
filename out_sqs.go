a2f58126b3485757969f8121304cb92931e6f73d
shaimoria <shai.moria@zooz.com>
2019-09-23 14:23:44 +0300

    yalla

--- out_sqs.go
+++ out_sqs.go
@@ -7,28 +7,22 @@ import (
 	"github.com/fluent/fluent-bit-go/output"
 )
 
-// FLBPluginRegister Gets called only once when the plugin.so is loaded
+//export FLBPluginRegister
 func FLBPluginRegister(def unsafe.Pointer) int {
 	return output.FLBPluginRegister(def, "sqs", "aws sqs output plugin")
 }
 
-// FLBPluginInit Gets called only once for each instance you have configured.
+//export FLBPluginInit
 func FLBPluginInit(plugin unsafe.Pointer) int {
-	// queueURL := output.FLBPluginConfigKey(plugin, "QueueURL")
-
-	// if queueURL == "" {
-	// 	return output.FLB_ERROR
-	// }
-
 	return output.FLB_OK
 }
 
-// FLBPluginFlushCtx Gets called with a batch of records to be written to an instance.
+//export FLBPluginFlushCtx
 func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int {
 	return output.FLB_OK
 }
 
-// FLBPluginExit Gets called when shuting down fluntBit
+//export FLBPluginExit
 func FLBPluginExit() int {
 	return output.FLB_OK
 }
